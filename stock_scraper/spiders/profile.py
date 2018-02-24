287da065a8350bbc984ba9e207f09bf3b47081fa
Tim Schruben <tim.schruben@gmail.com>
2018-02-24 15:40:57 -0500

    Lenghted the delay between requests

--- stock_scraper/spiders/profile.py
+++ stock_scraper/spiders/profile.py
@@ -20,7 +20,7 @@ class ProfileSpider(scrapy.Spider):
         symbolList = SymbolList(self.symbol)
 
         for symbolConfig in symbolList:
-            time.sleep(0.500)
+            time.sleep(1)
             yield scrapy.Request(
                     url = 'https://core-api.barchart.com/v1/quotes/get?fields=symbol%2CsymbolName%2Csectors%2Ceps&symbols=' + symbolConfig.symbol(),
                     meta = {'symbol': symbolConfig},
