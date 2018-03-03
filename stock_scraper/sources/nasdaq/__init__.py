47236ae9fcb620eacfb95e78305dea5dd6e8ccb1
Tim Schruben <tim.schruben@gmail.com>
2018-03-03 09:00:15 -0500

    Updating headers

--- stock_scraper/sources/nasdaq/__init__.py
+++ stock_scraper/sources/nasdaq/__init__.py
@@ -13,8 +13,8 @@ class Nasdaq(scrapy.Request):
                     callback = self.parse,
                     meta = {'config': symbolConfig},
                     headers = {
-                        'Accept-Encoding': 'gzip, deflate, sdch',
-                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                        'Accept-Encoding': 'gzip, deflate',
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
                         'Host': 'www.nasdaq.com',
                         'Referer': 'https://www.nasdaq.com/',
                         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
