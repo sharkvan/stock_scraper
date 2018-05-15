1f6b422c4eb79875ccb38b8361a75db121c8104c
Tim Schruben <tim.schruben@gmail.com>
2018-05-15 00:25:31 -0400

    Adjusting the price spider to not get banned

--- stock_scraper/spiders/price.py
+++ stock_scraper/spiders/price.py
@@ -26,7 +26,7 @@ class PriceSpider(scrapy.Spider):
                                         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
                                     })
 
-                thread.sleep(500)
+                thread.sleep(1000)
                 
     def parse(self, response):
         return self.parse_rows(response)
